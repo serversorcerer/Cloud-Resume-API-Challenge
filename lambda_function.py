4c1cc9712d8765cf5b60f14b552c49973efde9d9
serversorcerer <serversorcerer@gmail.com>
2024-06-29 18:21:04 -0400

    Add Lambda function and dependencies

---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('ResumeData')
+
+def lambda_handler(event, context):
+    try:
+        response = table.get_item(Key={'id': '1'})
+        resume_data = response.get('Item', {})
+        return {
+            'statusCode': 200,
+            'body': json.dumps(resume_data, indent=4)
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)}, indent=4)
+        }
