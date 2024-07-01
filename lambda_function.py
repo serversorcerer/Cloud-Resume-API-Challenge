b44132e5e75aafe7f4b125a62c5e3e59fbc97a43
serversorcerer <serversorcerer@gmail.com>
2024-07-01 11:32:38 -0400

    Updated lambda function and .ZIP list

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import boto3
+from collections import OrderedDict
 
-# Initialize DynamoDB resource and table
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('ResumeData')
 
@@ -9,16 +9,22 @@ def lambda_handler(event, context):
     try:
         response = table.get_item(Key={'id': '1'})
         resume_data = response.get('Item', {})
-        
-        json_resume_data = json.dumps(resume_data, indent=4, default=str)
-        
+
+        ordered_resume_data = OrderedDict([
+            ("id", resume_data.get("id")),
+            ("basics", resume_data.get("basics")),
+            ("certificates", resume_data.get("certificates")),
+            ("projects", resume_data.get("projects")),
+            ("skills", resume_data.get("skills")),
+        ])
+
         return {
             'statusCode': 200,
-            'body': json_resume_data
+            'body': json.dumps(ordered_resume_data, indent=4)
         }
     except Exception as e:
         print(e)
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
+            'body': json.dumps({'error': str(e)}, indent=4)
         }
