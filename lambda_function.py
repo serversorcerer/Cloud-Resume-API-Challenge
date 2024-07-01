66744769f59242c541f647fbf2c3d88151c7fb1d
serversorcerer <serversorcerer@gmail.com>
2024-07-01 14:16:54 -0400

    Updated lambda function and zip file

--- lambda_function.py
+++ lambda_function.py
@@ -1,30 +1,96 @@
-const AWS = require('aws-sdk');
+import json
+import boto3
 
-// Initialize AWS DynamoDB client
-const dynamodb = new AWS.DynamoDB();
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('ResumeData')
 
-exports.handler = async (event) => {
-    // Extract data from event
-    const { id, basics, certificates, projects, skills } = event;
-
-    // DynamoDB params for putting an item
-    const params = {
-        TableName: 'YourTableName', // Replace with your DynamoDB table name
-        Item: {
-            'id': { S: id },
-            'basics': { M: basics },
-            'certificates': { L: certificates },
-            'projects': { L: projects },
-            'skills': { L: skills }
+def lambda_handler(event, context):
+    try:
+        response = table.get_item(Key={'id': '1'})
+        resume_data = response.get('Item', {})
+        
+        ordered_resume_data = {
+            'id': resume_data.get('id', {}).get('S', ''),
+            'basics': {
+                'name': resume_data.get('basics', {}).get('M', {}).get('name', {}).get('S', ''),
+                'label': resume_data.get('basics', {}).get('M', {}).get('label', {}).get('S', ''),
+                'email': resume_data.get('basics', {}).get('M', {}).get('email', {}).get('S', ''),
+                'phone': resume_data.get('basics', {}).get('M', {}).get('phone', {}).get('S', ''),
+                'location': {
+                    'city': resume_data.get('basics', {}).get('M', {}).get('location', {}).get('M', {}).get('city', {}).get('S', ''),
+                    'region': resume_data.get('basics', {}).get('M', {}).get('location', {}).get('M', {}).get('region', {}).get('S', ''),
+                },
+                'profiles': [
+                    {
+                        'network': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[0].get('M', {}).get('network', {}).get('S', ''),
+                        'url': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[0].get('M', {}).get('url', {}).get('S', ''),
+                        'username': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[0].get('M', {}).get('username', {}).get('S', ''),
+                    },
+                    {
+                        'network': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[1].get('M', {}).get('network', {}).get('S', ''),
+                        'url': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[1].get('M', {}).get('url', {}).get('S', ''),
+                        'username': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[1].get('M', {}).get('username', {}).get('S', ''),
+                    },
+                    {
+                        'network': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[2].get('M', {}).get('network', {}).get('S', ''),
+                        'url': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[2].get('M', {}).get('url', {}).get('S', ''),
+                        'username': resume_data.get('basics', {}).get('M', {}).get('profiles', {}).get('L', [])[2].get('M', {}).get('username', {}).get('S', ''),
+                    },
+                ],
+                'summary': resume_data.get('basics', {}).get('M', {}).get('summary', {}).get('S', ''),
+                'url': resume_data.get('basics', {}).get('M', {}).get('url', {}).get('S', ''),
+            },
+            'certificates': [
+                {
+                    'name': resume_data.get('certificates', {}).get('L', [])[0].get('M', {}).get('name', {}).get('S', ''),
+                    'date': resume_data.get('certificates', {}).get('L', [])[0].get('M', {}).get('date', {}).get('S', ''),
+                    'issuer': resume_data.get('certificates', {}).get('L', [])[0].get('M', {}).get('issuer', {}).get('S', ''),
+                },
+                {
+                    'name': resume_data.get('certificates', {}).get('L', [])[1].get('M', {}).get('name', {}).get('S', ''),
+                    'date': resume_data.get('certificates', {}).get('L', [])[1].get('M', {}).get('date', {}).get('S', ''),
+                    'issuer': resume_data.get('certificates', {}).get('L', [])[1].get('M', {}).get('issuer', {}).get('S', ''),
+                },
+            ],
+            'projects': [
+                {
+                    'name': resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('name', {}).get('S', ''),
+                    'description': resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('description', {}).get('S', ''),
+                    'startDate': resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('startDate', {}).get('S', ''),
+                    'endDate': resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('endDate', {}).get('S', ''),
+                    'highlights': [
+                        resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('highlights', {}).get('L', [])[0].get('S', ''),
+                        resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('highlights', {}).get('L', [])[1].get('S', ''),
+                        resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('highlights', {}).get('L', [])[2].get('S', ''),
+                    ],
+                    'url': resume_data.get('projects', {}).get('L', [])[0].get('M', {}).get('url', {}).get('S', ''),
+                },
+                {
+                    'name': resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('name', {}).get('S', ''),
+                    'description': resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('description', {}).get('S', ''),
+                    'startDate': resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('startDate', {}).get('S', ''),
+                    'endDate': resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('endDate', {}).get('S', ''),
+                    'highlights': [
+                        resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('highlights', {}).get('L', [])[0].get('S', ''),
+                        resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('highlights', {}).get('L', [])[1].get('S', ''),
+                        resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('highlights', {}).get('L', [])[2].get('S', ''),
+                        resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('highlights', {}).get('L', [])[3].get('S', ''),
+                    ],
+                    'url': resume_data.get('projects', {}).get('L', [])[1].get('M', {}).get('url', {}).get('S', ''),
+                },
+            ],
+            'skills': [
+                {'name': skill.get('S', '')} for skill in resume_data.get('skills', {}).get('L', [])
+            ],
         }
-    };
 
-    try {
-        // Put item into DynamoDB
-        await dynamodb.putItem(params).promise();
-        return { statusCode: 200, body: 'Item added successfully' };
-    } catch (err) {
-        console.error('Error putting item into DynamoDB:', err);
-        return { statusCode: 500, body: 'Error putting item into DynamoDB' };
-    }
-};
+        return {
+            'statusCode': 200,
+            'body': json.dumps(ordered_resume_data, indent=4)
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)}, indent=4)
+        }
