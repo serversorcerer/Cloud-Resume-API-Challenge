6fa79356dc36c05e22512b266565bbfbf5148640
serversorcerer <serversorcerer@gmail.com>
2024-07-01 09:27:15 -0400

    Updated lambda function and created new function.zip

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+from collections import OrderedDict
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('ResumeData')
@@ -8,9 +9,19 @@ def lambda_handler(event, context):
     try:
         response = table.get_item(Key={'id': '1'})
         resume_data = response.get('Item', {})
+
+        ordered_resume_data = OrderedDict([
+            ("id", resume_data.get("id")),
+            ("basics", resume_data.get("basics")),
+            ("certificates", resume_data.get("certificates")),
+            ("projects", resume_data.get("projects")),
+            ("skills", resume_data.get("skills")),
+            ("work", resume_data.get("work"))
+        ])
+
         return {
             'statusCode': 200,
-            'body': json.dumps(resume_data, indent=4)
+            'body': json.dumps(ordered_resume_data, indent=4)
         }
     except Exception as e:
         print(e)
