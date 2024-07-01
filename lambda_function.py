5811f3aa1611606796163a1b133dad416822f376
serversorcerer <serversorcerer@gmail.com>
2024-07-01 14:27:18 -0400

    Updated lambda function and zip files

--- lambda_function.py
+++ lambda_function.py
@@ -13,9 +13,10 @@ def lambda_handler(event, context):
             }
         )
         resume_data = response.get('Item', {})
+        formatted_data = convert_dynamodb_to_json(resume_data)
         return {
             'statusCode': 200,
-            'body': json.dumps(resume_data, indent=4)
+            'body': json.dumps(formatted_data, indent=4)
         }
     except Exception as e:
         print(e)
@@ -23,3 +24,19 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps({'error': str(e)}, indent=4)
         }
+
+def convert_dynamodb_to_json(dynamodb_item):
+    def convert_value(value):
+        if isinstance(value, dict):
+            for key, val in value.items():
+                if key == 'S':
+                    return val
+                elif key == 'N':
+                    return int(val) if '.' not in val else float(val)
+                elif key == 'M':
+                    return convert_dynamodb_to_json(val)
+                elif key == 'L':
+                    return [convert_value(item) for item in val]
+        return value
+
+    return {k: convert_value(v) for k, v in dynamodb_item.items()}
