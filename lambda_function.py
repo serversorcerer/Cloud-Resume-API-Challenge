1cc50365bd4c85732fb1446b684421a10282b49e
serversorcerer <serversorcerer@gmail.com>
2024-07-01 11:26:10 -0400

    Updated lambda function and .ZIP to normal 2

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 
+# Initialize DynamoDB resource and table
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('ResumeData')
 
@@ -8,13 +9,16 @@ def lambda_handler(event, context):
     try:
         response = table.get_item(Key={'id': '1'})
         resume_data = response.get('Item', {})
+        
+        json_resume_data = json.dumps(resume_data, indent=4, default=str)
+        
         return {
             'statusCode': 200,
-            'body': json.dumps(resume_data, indent=4)
+            'body': json_resume_data
         }
     except Exception as e:
         print(e)
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': str(e)}, indent=4)
+            'body': json.dumps({'error': str(e)})
         }
