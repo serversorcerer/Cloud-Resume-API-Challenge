ae9e3968efe23e96ac085eb1d3eb4dd4d66e2d0c
serversorcerer <serversorcerer@gmail.com>
2024-07-02 12:01:26 -0400

    Updated lambda function and zip files

--- lambda_function.py
+++ lambda_function.py
@@ -7,29 +7,15 @@ table = dynamodb.Table('ResumeData')
 
 def lambda_handler(event, context):
     try:
-        response = table.get_item(Key={'id': {'S': '1'}})
+        response = table.get_item(Key={'id': '1'})
         resume_data = response.get('Item', {})
 
-        # Convert DynamoDB JSON format to standard JSON format
-        def convert_dynamodb_json(dynamodb_json):
-            if isinstance(dynamodb_json, dict):
-                if "S" in dynamodb_json:
-                    return dynamodb_json["S"]
-                elif "M" in dynamodb_json:
-                    return {k: convert_dynamodb_json(v) for k, v in dynamodb_json["M"].items()}
-                elif "L" in dynamodb_json:
-                    return [convert_dynamodb_json(v) for v in dynamodb_json["L"]]
-            return dynamodb_json
-
-        standard_json_data = convert_dynamodb_json(resume_data)
-
-        # Create ordered JSON
         ordered_resume_data = OrderedDict([
-            ("id", standard_json_data.get("id")),
-            ("basics", standard_json_data.get("basics")),
-            ("certificates", standard_json_data.get("certificates")),
-            ("projects", standard_json_data.get("projects")),
-            ("skills", standard_json_data.get("skills")),
+            ("id", resume_data.get("id")),
+            ("basics", resume_data.get("basics")),
+            ("certificates", resume_data.get("certificates")),
+            ("projects", resume_data.get("projects")),
+            ("skills", resume_data.get("skills")),
         ])
 
         return {
