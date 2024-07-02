4e0824581507ab8ba6521285456ee5d87c29d7c6
serversorcerer <serversorcerer@gmail.com>
2024-07-02 13:06:02 -0400

    Updated lambda function and zip files

--- lambda_function.py
+++ lambda_function.py
@@ -10,10 +10,29 @@ def lambda_handler(event, context):
         response = table.get_item(Key={'id': '1'})
         resume_data = response.get('Item', {})
 
+        basics = OrderedDict([
+            ("email", resume_data.get("basics", {}).get("M", {}).get("email", {}).get("S")),
+            ("phone", resume_data.get("basics", {}).get("M", {}).get("phone", {}).get("S")),
+            ("url", resume_data.get("basics", {}).get("M", {}).get("url", {}).get("S")),
+            ("summary", resume_data.get("basics", {}).get("M", {}).get("summary", {}).get("S")),
+            ("location", OrderedDict([
+                ("city", resume_data.get("basics", {}).get("M", {}).get("location", {}).get("M", {}).get("city", {}).get("S")),
+                ("region", resume_data.get("basics", {}).get("M", {}).get("location", {}).get("M", {}).get("region", {}).get("S")),
+                ("countryCode", resume_data.get("basics", {}).get("M", {}).get("location", {}).get("M", {}).get("countryCode", {}).get("S"))
+            ])),
+            ("profiles", [
+                OrderedDict([
+                    ("network", profile.get("M", {}).get("network", {}).get("S")),
+                    ("username", profile.get("M", {}).get("username", {}).get("S")),
+                    ("url", profile.get("M", {}).get("url", {}).get("S"))
+                ]) for profile in resume_data.get("basics", {}).get("M", {}).get("profiles", {}).get("L", [])
+            ])
+        ])
+
         ordered_resume_data = OrderedDict([
             ("id", resume_data.get("id")),
             ("name", resume_data.get("name")),
-            ("basics", resume_data.get("basics")),
+            ("basics", basics),
             ("certificates", resume_data.get("certificates")),
             ("projects", resume_data.get("projects")),
             ("skills", resume_data.get("skills")),
